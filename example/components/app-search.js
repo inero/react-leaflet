99a5bc69e463d147402513014ef0a90e8e9e5199
Immanuel <uneedimman@gmail.com>
2020-07-13 19:54:48 +0530

    feat: added zip serach to simple map

--- example/components/app-search.js
+++ example/components/app-search.js
@@ -3,15 +3,17 @@
 import Leaflet from 'leaflet';
 import React, { StrictMode } from 'react';
 
-import AppSearch from './app-search';
+import SimpleExample from './simple';
 
 Leaflet.Icon.Default.imagePath =
   '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/';
 
-const App = () => (
+const AppSearch = () => (
   <StrictMode>
-    <AppSearch/>
+    <h1>React-Leaflet examples</h1>
+    <h2>Popup with Marker</h2>
+    <SimpleExample />
   </StrictMode>
 )
 
-export default App;
+export default AppSearch;
--- example/components/simple.js
+++ example/components/simple.js
@@ -11,25 +11,38 @@ type State = {
 
 export default class SimpleExample extends Component<{}, State> {
   state = {
-    lat: 51.505,
-    lng: -0.09,
+    lat: 0,
+    lng: 0,
     zoom: 13,
+    zip: ''
+  }
+
+  input_onChange = (e) => {
+    console.log(e.target.value);
+    this.setState({zip: e.target.value, lat: 51.505, lng: -0.09});
+  }
+
+  input_onKeyDown = (e) => {
+    console.log(e.target.value);
   }
 
   render() {
-    const position = [this.state.lat, this.state.lng]
+    const position = [this.state.lat, this.state.lng];
     return (
-      <Map center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </Map>
+      <div align="center">
+        <input type="number" value={this.state.zip} onChange={this.input_onChange} onKeyDown={this.input_onKeyDown}></input>
+        <Map center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </Map>
+      </div>
     )
   }
 }
